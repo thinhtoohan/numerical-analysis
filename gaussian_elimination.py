d22157d6f675d058774abbf99ea96cf87f827112
thinhtoohan <thinhtoohan99@gmail.com>
2025-02-27 23:39:28 -0800

    final optimizations added

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -10,26 +10,26 @@ class hilbert_matrix:
             for j in range(0, n):
                 self.hilb_matrix[i][j] = np.around(1 / ((i + 1) + (j + 1) - 1),4)
         self.augmented_matrix = np.append(self.hilb_matrix, self.hilb_matrix @ self.true_x, axis=1)
+        '''
         print(self.augmented_matrix)
+        '''
     def gaussian_elimination(self):
         for i in range(0, self.n-1):
             pivot = self.augmented_matrix[i, i]
-            print(self.augmented_matrix)
             for j in range(i + 1, self.n):
                 multiplier = np.around(self.augmented_matrix[j, i] / pivot,4)
-                self.augmented_matrix[j,i]=0
-                print(f"{i}", multiplier)
+                self.augmented_matrix[j,i]=0 # items under pivots are zero without any calculation needed
                 self.multiplication_count += 1
-                print(np.around((-multiplier) * (self.augmented_matrix[i,i+1:]) + self.augmented_matrix[j,i+1:],4))
                 self.augmented_matrix[j,i+1:] = np.around((-multiplier) * (self.augmented_matrix[i,i+1:]) + self.augmented_matrix[j,i+1:],4)
                 self.multiplication_count += self.n-i
+        '''
         print(self.augmented_matrix)
+        '''
     def back_substitution(self):
-        n = self.augmented_matrix.shape[0]
-        self.x = np.zeros(n)
-        for i in range(n - 1, -1, -1):
-            self.x[i] = self.augmented_matrix[i, n]
-            for j in range(i + 1, n):
+        self.x = np.zeros(self.n)
+        for i in range(self.n - 1, -1, -1):
+            self.x[i] = self.augmented_matrix[i, self.n]
+            for j in range(i + 1, self.n):
                 self.x[i] -= self.augmented_matrix[i, j] * self.x[j]
                 self.multiplication_count += 1
             self.x[i] = self.x[i] / self.augmented_matrix[i, i]
@@ -44,15 +44,17 @@ class hilbert_matrix:
         print("Infinity Norm:\n",self.infinity_norm)
         print("L2 Norm:\n",self.l2_norm)
     def print_multiplication_count(self):
-        print("Multiplication Count:\n",self.multiplication_count)
+        print(f"Multiplication Count for n={self.n}:\n",self.multiplication_count)
 
+print("Hilbert Matrix with n=5")
 h_5 = hilbert_matrix(5)
 h_5.gaussian_elimination()
 h_5.back_substitution()
 h_5.calculate_error()
 h_5.calculate_norms()
 h_5.print_multiplication_count()
-'''
+
+print("Hilbert Matrix with n=11")
 h_11 = hilbert_matrix(11)
 h_11.gaussian_elimination()
 h_11.back_substitution()
@@ -60,6 +62,7 @@ h_11.calculate_error()
 h_11.calculate_norms()
 h_11.print_multiplication_count()
 
+print("Hilbert Matrix with n=12")
 h_12 = hilbert_matrix(12)
 h_12.gaussian_elimination()
 h_12.back_substitution()
@@ -67,10 +70,11 @@ h_12.calculate_error()
 h_12.calculate_norms()
 h_12.print_multiplication_count()
 
+print("Hilbert Matrix with n=13")
 h_13 = hilbert_matrix(13)
 h_13.gaussian_elimination()
 h_13.back_substitution()
 h_13.calculate_error()
 h_13.calculate_norms()
 h_13.print_multiplication_count()
-'''
+
