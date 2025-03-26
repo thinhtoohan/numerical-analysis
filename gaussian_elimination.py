d6c04df4e95c91f56a03ab82ae2c07a68244f8e2
thinhtoohan <thinhtoohan99@gmail.com>
2025-03-26 16:14:59 -0700

    non rounded test with n=3

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -10,9 +10,7 @@ class hilbert_matrix:
             for j in range(0, n):
                 self.hilb_matrix[i][j] = 1 / ((i + 1) + (j + 1) - 1)
         self.augmented_matrix = np.append(self.hilb_matrix, self.hilb_matrix @ self.true_x, axis=1)
-        '''
         print(self.augmented_matrix)
-        '''
     def gaussian_elimination(self):
         for i in range(0, self.n-1):
             pivot = self.augmented_matrix[i, i]
@@ -39,42 +37,34 @@ class hilbert_matrix:
         self.error = self.true_x.T-self.x
         print("Error:\n",self.error.flatten())
     def calculate_norms(self):
-        self.infinity_norm = np.around(np.linalg.norm(self.error.flatten(), np.inf),4)
-        self.l2_norm = np.linalg.norm(self.error) 
+        self.infinity_norm = np.linalg.norm(self.error.flatten(), np.inf)
+        self.l2_norm = np.around(np.linalg.norm(self.error),4)  
         print("Infinity Norm:\n",self.infinity_norm)
         print("L2 Norm:\n",self.l2_norm)
     def print_multiplication_count(self):
         print(f"Multiplication Count for n={self.n}:\n",self.multiplication_count)
 
-print("Hilbert Matrix with n=5")
-h_5 = hilbert_matrix(5)
-h_5.gaussian_elimination()
-h_5.back_substitution()
-h_5.calculate_error()
-h_5.calculate_norms()
-h_5.print_multiplication_count()
 
-print("Hilbert Matrix with n=11")
-h_11 = hilbert_matrix(11)
-h_11.gaussian_elimination()
-h_11.back_substitution()
-h_11.calculate_error()
-h_11.calculate_norms()
-h_11.print_multiplication_count()
+h_3 = hilbert_matrix(12)
+h_3.gaussian_elimination()
+h_3.back_substitution()
+h_3.calculate_error()
+h_3.calculate_norms()
+h_3.print_multiplication_count()
 
-print("Hilbert Matrix with n=12")
-h_12 = hilbert_matrix(12)
-h_12.gaussian_elimination()
-h_12.back_substitution()
-h_12.calculate_error()
-h_12.calculate_norms()
-h_12.print_multiplication_count()
 
-print("Hilbert Matrix with n=13")
-h_13 = hilbert_matrix(13)
-h_13.gaussian_elimination()
-h_13.back_substitution()
-h_13.calculate_error()
-h_13.calculate_norms()
-h_13.print_multiplication_count()
+# print("Hilbert Matrix with n=12")
+# h_12 = hilbert_matrix(12)
+# h_12.gaussian_elimination()
+# h_12.back_substitution()
+# h_12.calculate_error()
+# h_12.calculate_norms()
+# h_12.print_multiplication_count()
 
+# print("Hilbert Matrix with n=13")
+# h_13 = hilbert_matrix(13)
+# h_13.gaussian_elimination()
+# h_13.back_substitution()
+# h_13.calculate_error()
+# h_13.calculate_norms()
+# h_13.print_multiplication_count()
